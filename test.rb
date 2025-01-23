6565dd78860f103bd037b7cd6ef6ffc967ad9b79
syedanabimam <syedanabimam@gmail.com>
2025-01-24 02:02:35 +0500

    Add Expected Behavioral tests

---
+++ test.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+require 'minitest/autorun'
+
+class CheckoutTest < Minitest::Test
+  def setup
+    @checkout = Checkout.new(pricing_rules)
+  end
+
+  def test_case1
+    @checkout.scan('GR1')
+    @checkout.scan('SR1')
+    @checkout.scan('GR1')
+    @checkout.scan('GR1')
+    @checkout.scan('CF1')
+    assert_equal 22.45, @checkout.total
+  end
+
+  def test_case2
+    @checkout.scan('GR1')
+    @checkout.scan('GR1')
+    assert_equal 3.11, @checkout.total
+  end
+
+  def test_case3
+    @checkout.scan('SR1')
+    @checkout.scan('SR1')
+    @checkout.scan('GR1')
+    @checkout.scan('SR1')
+    assert_equal 16.61, @checkout.total
+  end
+
+  def test_case4
+    @checkout.scan('GR1')
+    @checkout.scan('CF1')
+    @checkout.scan('SR1')
+    @checkout.scan('CF1')
+    @checkout.scan('CF1')
+    assert_equal 30.57, @checkout.total
+  end
+end
