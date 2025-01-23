67da5d3aaeab7db347fefedee72bf5f9c2e740ba
syedanabimam <syedanabimam@gmail.com>
2025-01-24 03:10:03 +0500

    Condense Pricing Rules Hash based methods to lambda functions

--- checkout.rb
+++ checkout.rb
@@ -1,24 +1,18 @@
 class Checkout
-  def self.calculate_gr1_price(quantity)
-    price = 3.11
-    (quantity / 2 + quantity % 2) * price
-  end
-
-  def self.calculate_sr1_price(quantity)
-    price = quantity >= 3 ? 4.50 : 5.00
-    quantity * price
-  end
-
-  def self.calculate_cf1_price(quantity)
-    price = 11.23
-    price = (price * 2 / 3.0) if quantity >= 3
-    quantity * price
-  end
-
   PRICING_RULES = {
-    'GR1' => method(:calculate_gr1_price),
-    'SR1' => method(:calculate_sr1_price),
-    'CF1' => method(:calculate_cf1_price)
+    'GR1' => lambda do |quantity|
+      unit_price = 3.11
+      (quantity / 2 + quantity % 2) * unit_price
+    end,
+    'SR1' => lambda do |quantity|
+      unit_price = quantity >= 3 ? 4.50 : 5.00
+      quantity * unit_price
+    end,
+    'CF1' => lambda do |quantity|
+      unit_price = 11.23
+      unit_price = (unit_price * 2 / 3.0) if quantity >= 3
+      quantity * unit_price
+    end
   }.freeze
 
   def initialize(pricing_rules = PRICING_RULES)
